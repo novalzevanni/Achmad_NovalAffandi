efb14b06b0f343bbd82010d09fe902db181590a3
unknown <nofalgepeng925@gmail.com>
2023-04-05 15:14:53 +0700

    website coffe shop

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 //toggelclass aktif//
-const navbarNav = document.querySelector(".navbar-nav");
+const navbarNav = document.querySelector('.navbar-nav');
 
 //ketika humberger menu di klik
-document.querySelector("#hamburger-menu") .onclik = () => {
-  navbarNav.classList.toggle("active");
+document.querySelector('#hamburger-menu').onclick = () => {navbarNav.classList.toggle('active');
 };
 
 ///luar sidebar//
-const hamburger = document.querySelector ('#hamburger-menu');
+const hamburger = document.querySelector('#hamburger-menu');
 
-document.addEventListener('click', function(e){
-    if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active');
-    }
-})
+document.addEventListener('click', function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove('active');
+  }
+});
